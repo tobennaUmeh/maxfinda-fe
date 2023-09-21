6b6a4327215a32efac392ad0efdb437789b727c8
kelvincodex <morahthankgod@gmail.com>
2023-09-21 09:26:08 +0100

    lklk

--- src/service/BaseService.ts
+++ src/service/BaseService.ts
@@ -3,12 +3,29 @@ import {useSelector} from "react-redux";
 
 export const BaseService = () => {
     const token = useSelector<any>((state)=> state.auth.token)
-  return axios.create({
-      baseURL: "https://",
-      headers:{
-          "Content-Type": "application/json",
-          Accept:"application/json",
-          Authorization: `Bearer ${token}`
-      }
-  })
+    const axiosInstance =  axios.create({
+          baseURL: "https://",
+          headers:{
+              "Content-Type": "application/json",
+              Accept:"application/json",
+              Authorization: `Bearer ${token}`
+          }
+      })
+
+    axiosInstance.interceptors.response.use((response)=>{
+        console.log(response.data)
+        return response
+    },(error)=>{
+        console.log(error.message)
+        return Promise.reject(error)
+    })
+    axiosInstance.interceptors.request.use((config)=>{
+        console.log(config)
+        return config
+    }, (error)=> {
+        console.log(error)
+        return Promise.reject(error)
+    })
+
+    return axiosInstance
 }
